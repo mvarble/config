67e533c605d7aff08919e186580b7a37574b09cb
Matthew Varble <mrvarble@gmail.com>
2022-04-15 12:32:36 -0700

    added fish

---
+++ term/fish/config.fish
@@ -0,0 +1,34 @@
+# variables for software versions
+set NODE_VERSION 16.14.0
+set JULIA_VERSION 1.7.2
+set BLENDER_VERSION 3.0.1
+
+# clear the greeting
+set fish_greeting
+
+# use 256 colorscheme
+set TERM xterm-256color
+
+# update PATH
+set PATH ~/.node-v$NODE_VERSION/bin $PATH
+set PATH ~/.julia-$JULIA_VERSION/bin $PATH
+set PATH ~/.blender-$BLENDER_VERSION $PATH
+set PATH ~/.nvim/bin $PATH
+set PATH ~/.cargo/bin $PATH
+
+if test "$hostname" = "Kaitlynnes-MacBook-Air.local"
+  set PATH /opt/homebrew/bin $PATH
+  set OPEN open
+else
+  set PATH ~/.local/bin $PATH
+  set OPEN xdg-open
+end
+
+# allow node 8GB ram
+set -x NODE_OPTIONS '--max_old_space_size=8192'
+
+# Base16 Shell
+if status --is-interactive
+  set BASE16_SHELL "$HOME/.config/base16-shell/"
+  source "$BASE16_SHELL/profile_helper.fish"
+end
---
+++ term/fish/functions/oldvim.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function oldvim --wraps=vim --description 'alias oldvim=vim'
+  vim  $argv;
+end
