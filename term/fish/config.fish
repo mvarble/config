c0d0550cb70d1fba7a841ea67cd54e90801e12f0
Matthew Varble <mvarble@toyon.com>
2022-10-24 15:51:32 -0700

    added more coc extensions, updated vimrc, added fundle and ssh-agent

--- term/fish/config.fish
+++ term/fish/config.fish
@@ -7,7 +7,7 @@ set BLENDER_VERSION 3.0.1
 set fish_greeting
 
 # use 256 colorscheme
-set TERM xterm-256color
+set -x -U TERM xterm-256color
 
 # update PATH
 set PATH ~/.node-v$NODE_VERSION/bin $PATH
@@ -17,11 +17,18 @@ set PATH ~/.nvim/bin $PATH
 set PATH ~/.cargo/bin $PATH
 set PATH ~/.npm-global/bin $PATH
 
-# allow node 8GB ram
-set -x NODE_OPTIONS '--max_old_space_size=8192'
+# allow node 8GB ram in node.js
+set -x -U NODE_OPTIONS '--max_old_space_size=8192'
+
+# set GOPATH
+set -x -U GOPATH $HOME/.go
 
 # Base16 Shell
 if status --is-interactive
   set BASE16_SHELL "$HOME/.config/base16-shell/"
   source "$BASE16_SHELL/profile_helper.fish"
 end
+
+# plugins
+fundle plugin 'danhper/fish-ssh-agent'
+fundle init
