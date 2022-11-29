46d4aa3fe9bfc766d89d82edca4360ffc985b190
Matthew Varble <mvarble@toyon.com>
2022-11-28 16:37:55 -0800

    various updates; added zsh stuff

--- term/fish/config.fish
+++ term/fish/config.fish
@@ -1,3 +1,9 @@
+# start ssh server on login
+sudo service ssh start &>/dev/null
+
+# bind data sharing on login
+/usr/local/bin/share-data.sh
+
 # variables for software versions
 set NODE_VERSION 16.14.0
 set JULIA_VERSION 1.7.2
@@ -7,13 +13,16 @@ set BLENDER_VERSION 3.0.1
 set fish_greeting
 
 # update PATH
-set PATH ~/.node-v$NODE_VERSION/bin $PATH
-set PATH ~/.julia-$JULIA_VERSION/bin $PATH
-set PATH ~/.blender-$BLENDER_VERSION $PATH
-set PATH ~/.nvim/bin $PATH
-set PATH ~/.cargo/bin $PATH
-set PATH ~/.npm-global/bin $PATH
-set PATH ~/.local/bin $PATH
+set -x PATH ~/.node-v$NODE_VERSION/bin $PATH
+set -x PATH ~/.julia-$JULIA_VERSION/bin $PATH
+set -x PATH ~/.blender-$BLENDER_VERSION $PATH
+set -x PATH ~/.nvim/bin $PATH
+set -x PATH ~/.cargo/bin $PATH
+set -x PATH ~/.npm-global/bin $PATH
+set -x PATH ~/.local/bin $PATH
+
+# set editor to neovim
+set -x EDITOR nvim
 
 # allow node 8GB ram in node.js
 set -x NODE_OPTIONS '--max_old_space_size=8192'
@@ -30,7 +39,3 @@ end
 # plugins
 fundle plugin 'danhper/fish-ssh-agent'
 fundle init
-
-# set DISPLAY
-set -x LIBGL_ALWAYS_INDIRECT 1
-set -x DISPLAY localhost:10.0
