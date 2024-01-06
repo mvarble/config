7c21b897e90e531ed5e1ab145d6230921bdeb047
Matthew Varble <matthew@rodent.club>
2024-01-05 19:03:17 -0800

    updated lazy deps; turned off preselect so that rust is not annoying

--- editor/lvim/lua/user/lsp/rust.lua
+++ editor/lvim/lua/user/lsp/rust.lua
@@ -5,6 +5,7 @@ table.insert(lvim.builtin.treesitter.ensure_installed, "toml")
 -- plugins
 vim.list_extend(lvim.plugins, {
   "simrat39/rust-tools.nvim",
+  "alx741/vim-rustfmt",
   {
     "saecki/crates.nvim",
     version = "v0.3.0",
@@ -22,6 +23,8 @@ vim.list_extend(lvim.plugins, {
     end,
   },
 })
+
+lvim.builtin.cmp.preselect = false
 
 
 -- manually configure rust_analyzer
@@ -70,26 +73,3 @@ pcall(function()
     },
   }
 end)
-
-vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
-
-lvim.builtin.which_key.mappings["C"] = {
-  name = "Rust",
-  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
-  t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
-  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
-  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
-  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
-  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
-  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
-  R = {
-    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
-    "Reload Workspace",
-  },
-  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
-  y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
-  P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
-  i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
-  f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
-  D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
-}
