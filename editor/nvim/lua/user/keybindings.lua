ad05026c21df5e17980a50610a1efb3242b1aa2d
Matthew Varble <matthew@rodent.club>
2024-08-30 12:47:01 -0700

    fixed annoyances with autocomplete and freezing during documentation-lookup; added back buffer line; added keyboard commands for buffer management

--- editor/nvim/lua/user/keybindings.lua
+++ editor/nvim/lua/user/keybindings.lua
@@ -2,11 +2,16 @@
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
--- moving windows
+-- window navigation
 vim.keymap.set("n", "gh", ":wincmd h <CR>", { desc = "navigate window left" })
 vim.keymap.set("n", "gj", ":wincmd j <CR>", { desc = "navigate window down" })
 vim.keymap.set("n", "gk", ":wincmd k <CR>", { desc = "navigate window up" })
 vim.keymap.set("n", "gl", ":wincmd l <CR>", { desc = "navigate window right" })
+
+-- buffer navigation
+vim.keymap.set("n", "H", ":bp <CR>", { desc = "previous buffer" })
+vim.keymap.set("n", "L", ":bn <CR>", { desc = "next buffer" })
+vim.keymap.set("n", "<C-d>", ":bp<bar>sp<bar>bn<bar>bd<CR> <CR>", { desc = "close buffer" })
 
 -- closing windows
 vim.keymap.set("n", "<leader>q", ":q <CR>", { desc = "quit" })
--- editor/nvim/lua/user/plugins/bufferline.lua
+++ editor/nvim/lua/user/plugins/bufferline.lua
@@ -1,5 +1,16 @@
 return {
 	"akinsho/bufferline.nvim",
 	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
+	dependencies = { "nvim-tree/nvim-web-devicons", "RRethy/base16-nvim" },
+	config = function()
+		vim.opt.termguicolors = true
+		local base_bg = require("base16-colorscheme").colors.base00
+		require("bufferline").setup({
+			highlights = {
+				fill = {
+					bg = require("bufferline.colors").shade_color(base_bg, -25),
+				},
+			},
+		})
+	end,
 }
