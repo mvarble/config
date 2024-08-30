23a764b3e46627cb9d8bfc31e6a7f36e961bea50
Matthew Varble <matthew@rodent.club>
2024-08-29 20:35:16 -0700

    removed lunarvim from config

--- editor/lvim/lua/user/lsp/init.lua
+++
@@ -1,5 +0,0 @@
-lvim.format_on_save.enabled = true
-lvim.builtin.autopairs.active = false
-reload('user.lsp.python')
-reload('user.lsp.node')
-reload('user.lsp.rust')
--- term/fish/config.fish
+++ term/fish/config.fish
@@ -1,35 +1,25 @@
-# variables for software versions
-set NODE_VERSION 18.12.1
-set JULIA_VERSION 1.8.3
-
 # clear the greeting
 set fish_greeting
 
 # update PATH
-set -x PATH ~/.cargo/bin $PATH
-set -x PATH ~/.npm-global/bin $PATH
-set -x PATH ~/.local/bin $PATH
-set -x PATH ~/.pixi/bin $PATH
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.npm-global/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.pixi/bin
 
 # set editor to neovim and alias
-set -x EDITOR lvim
+set -x EDITOR nvim
 alias oldvim="vim"
-alias vim="lvim"
-alias vi="lvim"
-
-# allow node 8GB ram in node.js
-set -x NODE_OPTIONS '--max_old_space_size=8192'
-
-# set GOPATH
-set -x GOPATH $HOME/.go
+alias vim="nvim"
+alias vi="nvim"
 
 # Base16 Shell
 if status --is-interactive
-  set BASE16_SHELL "$HOME/.config/base16-shell/"
-  source "$BASE16_SHELL/profile_helper.fish"
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
 end
 
 # plugins
-fundle plugin 'danhper/fish-ssh-agent'
-fundle plugin 'edc/bass'
+fundle plugin danhper/fish-ssh-agent
+fundle plugin edc/bass
 fundle init
