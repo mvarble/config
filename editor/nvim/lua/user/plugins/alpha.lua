82aa4d23377f74c2063b79a4730f52130b7a7432
Matthew Varble <matthew@rodent.club>
2024-09-24 10:35:05 -0700

    cleaned up splash screen

--- editor/nvim/lua/user/plugins/alpha.lua
+++ editor/nvim/lua/user/plugins/alpha.lua
@@ -52,12 +52,12 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+			dashboard.button("e", "  > New file", "<cmd>ene<CR>"),
 			dashboard.button("<leader>e", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("<leader>ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("<leader>fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("<leader>fp", " > Projects", "<cmd>Telescope projects<CR>"),
-			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("<leader>ff", "󰱼  > Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("<leader>fs", "  > Find word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("<leader>fp", "  > Projects", "<cmd>Telescope projects<CR>"),
+			dashboard.button("q", "  > Quit neovim", "<cmd>qa<CR>"),
 		}
 
 		-- Send config to alpha
