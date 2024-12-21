d94a6e6b99fb99763c703fc484ba833e9544c066
Matthew Varble <matthew@rodent.club>
2024-12-21 09:43:32 -0800

    added md preview

--- editor/nvim/lua/user/plugins/markdown-preview.lua
+++ editor/nvim/lua/user/plugins/markdown-preview.lua
@@ -5,6 +5,15 @@ return {
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
         vim.g.mkdp_auto_close = 0
+        vim.g.mkdp_preview_options = {
+            katex = {
+                delimeters = {
+                    { left = "$`", right = "`$", display = false },
+                    { left = "$$", right = "$$", display = true },
+                    { left = "```math", right = "```", display = true },
+                },
+            },
+        }
     end,
     ft = { "markdown" },
 }
