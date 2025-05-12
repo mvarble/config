74a3909cde99ef125dd79b513e3e4593412f916b
Matthew Varble <matthew@rodent.club>
2025-05-12 11:19:17 -0700

    Deleted all of the GUI stuff that I no longer use or have any intuition for; moved `editor/nvim` -> `nvim`.

--- gui/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Terminate already running bar instances
-polybar-msg cmd quit
-
-#Launch main bar
-polybar --reload main 2>&1 | tee -a /tmp/polybar-main.log & disown
-polybar --reload alt 2>&1 | tee -a /tmp/polybar-alt.log & disown
