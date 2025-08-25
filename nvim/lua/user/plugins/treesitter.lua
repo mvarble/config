c2562e6c114189ee06a742215c52da930da7fbee
Matthew Varble <matthew@rodent.club>
2025-08-25 11:02:06 -0700

    Disabled treesitter after lines are too long

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -32,6 +32,11 @@ return {
         treesitter.setup({
             highlight = {
                 enable = true,
+                disable = function(_, bufnr)
+                    local buf_name = vim.api.nvim_buf_get_name(bufnr)
+                    local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+                    return file_size > 256 * 1024
+                end,
             },
             indent = { enable = true, additional_vim_regex_highlighting = false },
             autotag = {
