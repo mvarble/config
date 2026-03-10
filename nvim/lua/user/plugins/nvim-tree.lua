b981ac00f04c8dd8e99373b6f22f297316c6fdbc
Matthew Varble <matthew@rodent.club>
2026-03-10 02:11:37 -0700

    Pulled changes from main

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,5 +1,6 @@
 return {
     "nvim-tree/nvim-tree.lua",
+    version = "v1.15.0",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
         local nvimtree = require("nvim-tree")
